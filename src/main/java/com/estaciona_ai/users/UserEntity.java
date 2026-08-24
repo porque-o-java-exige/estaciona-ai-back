063e63fb868a8acdd453aaf198ce61ef29cf9e67
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-08-24 14:08:29 -0300

    feat: add user entity

--- src/main/java/com/estaciona_ai/users/UserEntity.java
+++ src/main/java/com/estaciona_ai/users/UserEntity.java
@@ -1,4 +1,32 @@
 package com.estaciona_ai.users;
 
+import com.estaciona_ai.enums.RolesEnum;
+import com.estaciona_ai.enums.StatusEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String phoneNumber;
+    /*private String photoUrl;*/
+    @Enumerated(EnumType.STRING)
+    private RolesEnum roles;
+    @Enumerated(EnumType.STRING)
+    private StatusEnum status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
