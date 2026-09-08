4c2af0b15a23df10a74d779f1a2e91f0f78d8f4b
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-08 15:25:17 -0300

    feat: add getters and setters

--- src/main/java/com/estaciona_ai/users/UserEntity.java
+++ src/main/java/com/estaciona_ai/users/UserEntity.java
@@ -4,13 +4,16 @@ import com.estaciona_ai.enums.RolesEnum;
 import com.estaciona_ai.enums.StatusEnum;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "users")
 public class UserEntity {
