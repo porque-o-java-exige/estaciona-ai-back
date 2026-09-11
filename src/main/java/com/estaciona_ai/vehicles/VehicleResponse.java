c29f321d2aec1b3c604a13b3526c67851f508fe5
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-11 11:07:52 -0300

    feat: add vehicle record

--- src/main/java/com/estaciona_ai/vehicles/VehicleResponse.java
+++ src/main/java/com/estaciona_ai/vehicles/VehicleResponse.java
@@ -1,28 +1,16 @@
 package com.estaciona_ai.vehicles;
 
-import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
-public class VehicleResponse {
-
-    private UUID id;
-    private UUID userId;
-    private String licensePlate;
-    private Integer year;
-    private String model;
-    private String color;
-    private LocalDateTime createdAt;
-
-    public VehicleResponse(VehicleEntity vehicle) {
-        this.id = vehicle.getId();
-        this.userId = vehicle.getUser().getId();
-        this.licensePlate = vehicle.getLicensePlate();
-        this.year = vehicle.getYear();
-        this.model = vehicle.getModel();
-        this.color = vehicle.getColor();
-        this.createdAt = vehicle.getCreatedAt();
-    }
+public record VehicleResponse(
+        UUID id,
+        UUID userId,
+        String licensePlate,
+        Integer year,
+        String model,
+        String color,
+        LocalDateTime createdAt
+) {
 }
