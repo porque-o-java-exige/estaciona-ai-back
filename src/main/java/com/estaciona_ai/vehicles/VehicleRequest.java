37e9ff2f480d992b7736aea037fbdd1e9a77f169
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-11 11:07:34 -0300

    feat: add vehicle record

--- src/main/java/com/estaciona_ai/vehicles/VehicleRequest.java
+++ src/main/java/com/estaciona_ai/vehicles/VehicleRequest.java
@@ -1,18 +1,25 @@
 package com.estaciona_ai.vehicles;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import jakarta.validation.constraints.*;
 
-@Data
-public class VehicleRequest {
+public record VehicleRequest(
 
-    @NotBlank
-    private String licensePlate;
+        @NotBlank(message = "A placa é obrigatória")
+        @Size(min =7, max = 8)
+        @Pattern(regexp = "^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$", message = "Formato de placa inválido")
+        String licensePlate,
+        @NotNull(message = "O ano é obrigatório")
+        @Min(value = 1900, message = "Ano inválido")
+        Integer year,
+        @NotBlank(message = "A marca é obrigatória")
+        @Size(max = 50, message = "A marca deve ter no máximo 50 caracteres")
+        String brand,
+        @NotBlank(message = "O modelo é obrigatório")
+        @Size(max = 100, message = "O modelo deve ter no máximo 100 caracteres")
+        String model,
+        @NotBlank(message = "A cor é obrigatória")
+        @Size(max = 50, message = "A cor deve ter no máximo 50 caracteres")
+        String color
+) {
 
-    private Integer year;
-
-    private String model;
-
-    private String color;
 }
