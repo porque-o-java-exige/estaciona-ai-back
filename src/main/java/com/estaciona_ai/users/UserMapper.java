1a81c15c6526841f5f775e5b385d5bef860b1f77
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2026-09-11 09:18:17 -0300

    Revert "[ FEAT ] add user CRUD"

--- src/main/java/com/estaciona_ai/users/UserMapper.java
+++ src/main/java/com/estaciona_ai/users/UserMapper.java
@@ -1,18 +1,4 @@
 package com.estaciona_ai.users;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import java.util.List;
-
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    //Request to entity
-    UserEntity toEntity(UserRequest userReq);
-    //Response to list
-    List<UserResponse> toResponseList(List<UserEntity> entities);
-    //Entity to response
-    UserResponse toResponse(UserEntity userEnt);
-
-    void updateEntityFromDto(UserRequest userReq, @MappingTarget UserEntity userEntity);
-
+public class UserMapper {
 }
