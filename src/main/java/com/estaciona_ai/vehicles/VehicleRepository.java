8fcbd937a2d2eda2944cb0333033a71849571284
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-11 17:36:34 -0300

    feat: add exists by license plate method

--- src/main/java/com/estaciona_ai/vehicles/VehicleRepository.java
+++ src/main/java/com/estaciona_ai/vehicles/VehicleRepository.java
@@ -2,9 +2,12 @@ package com.estaciona_ai.vehicles;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<VehicleEntity, UUID> {
+
+    boolean existsByLicensePlate(String licensePlate);
+    List<VehicleEntity> findByOwnerId(UUID ownerId);
 }
