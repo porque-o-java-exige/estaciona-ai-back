dca1065e7af56d96ec856b044bb2250b321ea320
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-11 11:07:21 -0300

    feat: add vehicle brand

--- src/main/java/com/estaciona_ai/vehicles/VehicleEntity.java
+++ src/main/java/com/estaciona_ai/vehicles/VehicleEntity.java
@@ -2,36 +2,38 @@ package com.estaciona_ai.vehicles;
 
 import com.estaciona_ai.users.UserEntity;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "vehicles")
 public class VehicleEntity {
 
     @Id
-    @UuidGenerator
-    @Column(length = 36)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private UserEntity user;
+    private UserEntity owner;
 
-    @Column(name = "license_plate", length = 20, nullable = false, unique = true)
+    @Column(name = "license_plate", nullable = false, unique = true)
     private String licensePlate;
     private int year;
 
-    @Column(length = 100)
     private String model;
 
-    @Column(length = 50)
     private String color;
+
+    private String brand;
 
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
