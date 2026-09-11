561400a04a6796e5c0e3422c380f5bcba50d7661
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-11 17:36:52 -0300

    feat: add brand and owner id

--- src/main/java/com/estaciona_ai/vehicles/VehicleResponse.java
+++ src/main/java/com/estaciona_ai/vehicles/VehicleResponse.java
@@ -6,11 +6,11 @@ import java.util.UUID;
 
 public record VehicleResponse(
         UUID id,
-        UUID userId,
+        UUID ownerId,
         String licensePlate,
         Integer year,
+        String brand,
         String model,
-        String color,
-        LocalDateTime createdAt
+        String color
 ) {
 }
