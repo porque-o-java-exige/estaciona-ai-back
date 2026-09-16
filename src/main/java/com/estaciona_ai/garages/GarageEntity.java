76f6b8c38a47e4b916683fb9a53a3454f8686e2b
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-16 15:47:54 -0300

    feat: add garages features list

--- src/main/java/com/estaciona_ai/garages/GarageEntity.java
+++ src/main/java/com/estaciona_ai/garages/GarageEntity.java
@@ -3,22 +3,27 @@ package com.estaciona_ai.garages;
 import com.estaciona_ai.users.UserEntity;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "garages")
 public class GarageEntity {
     @Id
     @UuidGenerator
     private UUID id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity owner;
     private String name;
@@ -27,16 +32,18 @@ public class GarageEntity {
     private Double longitude;
     @Column(columnDefinition = "TEXT")
     private String description;
-    private String photo;
-    private BigDecimal price;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "garage_features", joinColumns = @JoinColumn(name = "garage_id"))
+    @Column(name = "feature", nullable = false, length = 50)
+    private Set<String> features = new HashSet<>();
     @Column(nullable = false)
     private Boolean available;
+    private BigDecimal pricePerDay;
+    private BigDecimal pricePerHour;
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-    private BigDecimal pricePerDay;
-    private BigDecimal pricePerHour;
 }
